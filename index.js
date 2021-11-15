536490ba581781107b80fa881c0bf674a12cf9cf
BernardKonan <bernardkonan96@gmail.com>
2021-11-15 15:28:22 +0000

    code based commited

---
+++ index.js
@@ -0,0 +1,26 @@
+// // learn js from scratch
+// console.log("it's work"); // in double quotes;
+// console.log('Hello, world !'); // in simple quotes
+// console.log(`In backtick quotes`); // in backtick quotes
+
+
+/* artihmetic operations */
+
+console.log('2 + 3 = ',2 + 5); // addition
+console.log('4 - 2 = ',4 - 2); // substraction
+console.log('4 x 6 = ', 4 * 6); // multiplication
+console.log('25 / 4 = ', 25 / 4);// division 
+console.log('12 % 5 = ', 12%5); // Modulus - finding remainder
+console.log('3^2 = ', 3**2); // Exponentiation
+
+/* datatypes */
+let number1 = 12;
+let floatNumber = 12.67;
+
+let nom = "bernard konan";
+let greeting = `Hello ${nom}`;
+let message = 'Bye bye';
+
+let firstname; // undefined value
+let lastname = null; // empty value
+
