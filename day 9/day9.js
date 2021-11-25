b1d3fdea7642041ffe46f50a8a34cf5646a8e74a
bernardkonan <bernardkonan96@gmail.com>
2021-11-25 12:56:41 +0100

    end of chapter 9

--- day 9/day9.js
+++ day 9/day9.js
@@ -81,15 +81,175 @@
     })
 */
 
-const artists = ["Ninho", "Adele", "Niska", "Aya Nakamura", "John Legend"];
+const artists = ["NiNho", "Adele", "Niska", "Aya Nakamura", "John Legend"];
 artists.forEach((artist, index) => {
   console.log(`Artist N ${index + 1} - ${artist}`);
 });
 
 // transform each country in uppercase character
-const countries = ["Finland", "Denmark", "Sweden", "Norway", "Iceland"];
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
 countries.forEach((country) => {
   console.log("\n" + country + "\n");
 });
 
 // map -> Iterate an array elements and modify the array elements
+
+// capitalize a string
+const capitalize = (string) => {
+  const lower = string.toLowerCase();
+  return lower[0].toUpperCase() + lower.slice(1, lower.length);
+};
+
+artists.map((artist) => {
+  console.log(capitalize(artist));
+});
+
+const numbers = [1, 8, 5, 9];
+const square = numbers.map((number) => number * number);
+console.log(square);
+
+// -------------------------------------------------------
+const countriesToUppercase = countries.map((country) => country.toUpperCase());
+const countryThreeFirstLetters = countriesToUppercase.map((country) =>
+  country.slice(0, 3)
+);
+console.log(countriesToUppercase);
+console.log(countryThreeFirstLetters);
+
+// filter -> Filter out items which full fill filtering conditions and return a new array.
+
+// filter countries containing land
+const countriesContainingLand = countries.filter((country) => {
+  return country.includes("land");
+});
+console.log(countriesContainingLand); // [ 'Finland', 'Ireland' ]
+
+const countriesEndsByia = countries.filter((country) => {
+  return country.endsWith("ia");
+});
+console.log(countriesEndsByia); // [ 'Albania', 'Bolivia', 'Ethiopia' ]
+
+const countriesShortName = countries.filter((country) => country.length <= 5); // [ 'Japan', 'Kenya' ]
+console.log(countriesShortName);
+
+const scores = [
+  { name: "Asabeneh", score: 95 },
+  { name: "Lidiya", score: 98 },
+  { name: "Mathias", score: 80 },
+  { name: "Elias", score: 50 },
+  { name: "Martha", score: 85 },
+  { name: "John", score: 100 },
+];
+
+const scoresGreaterEight = scores.filter((score) => {
+  return score.score >= 80;
+});
+console.log(scoresGreaterEight);
+
+scoresGreaterEight.forEach((score, index) => {
+  console.log(`N${index}: score ${score.name} - ${score.score}`);
+});
+
+// reduce
+/* 
+  The call back function takes accumulator, current, and optional initial value as a parameter
+  and returns a single value
+*/
+
+const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+console.log(`Sum values : ${sum}`);
+
+const times = numbers.reduce((acc, cur) => {
+  return acc * cur;
+}, 1);
+console.log(`Times values : ${times}`);
+
+// every -> Check if all the elements are similar in one aspect. It returns boolean
+const names = ["Asabeneh", "Mathias", "Elias", "Brook"];
+const areAllStr = names.every((name) => {
+  return typeof name === "string";
+});
+console.log(areAllStr); // true
+
+const bools = [true, true, true, true];
+const areAllTrue = bools.every((b) => b === true);
+console.log(areAllTrue); // true
+
+const ages = [24, 22, 25, 32, 35, 18];
+const areAllAdult = ages.every((age) => age >= 18);
+console.log(areAllAdult); // true
+
+// find -> return the first element which satisfies the condition
+
+const age = ages.find((age) => age <= 20);
+console.log(age); // 18
+
+const user = scores.find((user) => user.score >= 85);
+console.log(user); //{ name: 'Asabeneh', score: 95 }
+
+// findIndex -> Return the position of the first element which satisfies the condition
+const nums = [24, 22, 25, 32, 35, 18];
+const index = nums.findIndex((num) => num >= 25);
+console.log(index);
+
+// some is almost similar to every
+const booleans = [true, true, true, true];
+
+const areSomeTrue = booleans.some((b) => b === true);
+
+console.log(areSomeTrue); //true
+
+// sort
+/* The sort methods arranges the array elements either ascending or descending order */
+
+// Sorting string  values
+const products = ["Milk", "Coffee", "Sugar", "Honey", "Apple", "Carrot"];
+console.log(products.sort());
+
+// sorting numeric values
+const numbs = [9.81, 3.14, 100, 37];
+// ascending
+numbs.sort((a, b) => {
+  return a - b;
+});
+console.log(numbs);
+// descending
+numbs.sort((a, b) => {
+  return b - a;
+});
+console.log(numbs);
+
+// sorting Object Arrays
+
+const users2 = [
+  { name: "Asabeneh", age: 150 },
+  { name: "Brook", age: 50 },
+  { name: "Eyo", age: 100 },
+  { name: "Elias", age: 22 },
+];
+// descending
+users2.sort((a, b) => {
+  if (a.age < b.age) return 1;
+  if (a.age > b.age) return -1;
+  return 0;
+});
+console.log(users2);
+// ascending
+users2.sort((a, b) => {
+  if (a.age < b.age) return -1;
+  if (a.age > b.age) return 1;
+  return 0;
+});
+console.log(users2);
