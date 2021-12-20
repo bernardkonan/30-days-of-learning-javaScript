4a3de58fa3316db5dbdcbd9ba2269403ebeac963
bernardkonan <bernardkonan96@gmail.com>
2021-12-20 17:57:10 +0100

    beginning of chapter 18

---
+++ day 18/day18.js
@@ -0,0 +1,23 @@
+/* LEARN ABOUT PROMISE */
+ // -> Way to handle asynchronous operations in javascript
+
+ // Promise constructor
+  const skills = ['HTML', 'CSS', 'JS'];
+
+  const doPromise = new Promise((res,rej)=>{
+      setTimeout(()=>{
+          if(skills.length > 0){
+              res(skills)
+          }else if (skills.includes('Node')){
+            res('Fullstack developper');
+          }else{
+              rej('Something wrong was append !');
+          }
+      }, 3000)
+  })
+
+  doPromise.then(value => {
+      value.forEach(el => {
+          console.log(el);
+      });
+  }).catch(err => console.log(err.message));
