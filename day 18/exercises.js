2c9ee1c1613e1aa3eb58ac3ddc698e56a0962e01
bernardkonan <bernardkonan96@gmail.com>
2021-12-25 11:22:29 +0000

    end of chapter 18

--- day 18/exercises.js
+++ day 18/exercises.js
@@ -1,10 +1,18 @@
-// Exercise 1
-const countriesAPI = 'https://restcountries.com/v2/all'
-const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+// // Exercise 1
+// const countriesAPI = 'https://restcountries.com/v2/all'
+// const catsAPI = 'https://api.thecatapi.com/v1/breeds'
 // // 1 -
 // fetch(countriesAPI)
 //     .then(resp => resp.json())
-//     .then(json => console.log([json[0].name, json[0].capital, json[0].languages, json[0].population, json[0].area]));
+//     .then(json => {
+//         console.log([
+//             json[0].name,
+//             json[0].capital,
+//             json[0].languages,
+//             json[0].population,
+//             json[0].area
+//         ])
+//     });
 
 // // 1-1 
 // const fetchCountries = async (urlAPI) => {
@@ -15,7 +23,7 @@ const catsAPI = 'https://api.thecatapi.com/v1/breeds'
 // }
 // fetchCountries(countriesAPI);
 
-// 2 -
+// // 2 -
 
 // fetch(catsAPI)
 //     .then(resp => resp.json())
@@ -27,39 +35,90 @@ const catsAPI = 'https://api.thecatapi.com/v1/breeds'
 //         })
 //     })
 
-// 3
+// // 3
 
-const findAverage = async (url) => {
-    const resp = await fetch(url)
-    const json = await resp.json();
-    return json;
-}
+// const findAverage = async (url) => {
+//     const resp = await fetch(url)
+//     const json = await resp.json();
+//     return json;
+// }
 
-const findLargestCountries = () => {
-    findAverage(catsAPI)
-        .then(json => console.log(json[0].weight))
-}
+// const findLargestCountries = () => {
+//     findAverage(catsAPI)
+//         .then(json => console.log(json[0].weight))
+// }
 
-findLargestCountries(catsAPI);
+// findLargestCountries(catsAPI);
 
-// closures
+// // closures
 
-const makeOperations = () => {
-    const result = 10;
-    const times = () => {
-        return 10 * result;
+// const makeOperations = () => {
+//     const result = 10;
+//     const times = () => {
+//         return 10 * result;
+//     }
+//     return result + times();
+// }
+
+// makeOperations();
+
+
+// const api = (url) => {
+//     const res = async (url) => {
+//         return await fetch(url).then(res => res.json())
+//     }
+//     res(url).then(json => console.log(json))
+// }
+
+// api(catsAPI);
+
+
+// // Ecrire une fonction qui généère des couleurs aléatoires
+
+// const hexColor = () => {
+//     const base = '0123456789ABCDEF';
+//     let hex = '#';
+//     for (let i = 0; i < 6; i++) {
+//         const index = Math.floor(Math.random() * base.length);
+//         hex += base[index];
+//     }
+//     return hex;
+// }
+
+// console.log(hexColor());
+
+// Une fonction qui affiche l'heure et la date
+
+const getFullDateAndHour = () => {
+    const now = new Date();
+    // date
+    const year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let day = now.getUTCDate();
+    // getHour
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    // conditions
+    if (month < 10) {
+        month = '0' + month;
     }
-    return result + times();
-}
-
-makeOperations();
-
-
-const api = (url) => {
-    const res = async (url) => {
-        return await fetch(url).then(res => res.json())
+    if (day < 10) {
+        day = '0' + day;
     }
-    res(url).then(json => console.log(json))
-}
+    if (hour < 10) {
+        hour = '0' + hour;
+    }
+    if (minute < 10) {
+        minute = '0' + minute;
+    }
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+    const fullDate = `${day}/${month}/${year}`;
+    const fullTime = `${hour}h ${minute}min ${seconds}s`;
 
-api(catsAPI);
+    return fullDate + ' ' + fullTime;
+}
+console.log(getFullDateAndHour());
